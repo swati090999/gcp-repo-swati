ea5e59b882242a51a5cc32118511ab15e78cb784
ITS Student <swatitaank99@gmail.com>
2026-06-05 05:51:05 +0000

    Initial commit: Node.js app with Dockerfile and cloudbuild.yaml

---
+++ app.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const PORT = process.env.PORT || 8080;
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('ITS Tech School -- GCP ACE Training v1.0\n');
+}).listen(PORT, () => console.log(`Listening on ${PORT}`));
