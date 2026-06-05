ee5314e5334f48518aed4a5817b80887281f61aa
ITS Student <swatitaank99@gmail.com>
2026-06-05 06:21:05 +0000

    Update to v2.0 -- trigger CI/CD pipeline

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const http = require('http');
 const PORT = process.env.PORT || 8080;
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('ITS Tech School -- GCP ACE Training v1.0\n');
+  res.end('ITS Tech School -- GCP ACE Training v2.0\n');
 }).listen(PORT, () => console.log(`Listening on ${PORT}`));
